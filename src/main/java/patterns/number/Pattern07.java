4f0b9d389364cc722cd85160ae00a47db9541aca
AkshayM7 <akshay.k.mache@gmail.com>
2025-11-27 19:45:28 +0530

    add number pattern pattern07

---
+++ src/main/java/patterns/number/Pattern07.java
@@ -0,0 +1,26 @@
+package patterns.number;
+
+/* Write a java program to print below pattern
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+6 5 4 3 2 1
+7 6 5 4 3 2 1
+*/
+public class Pattern07 {
+    public static void main(String[] args) {
+        int rows = 7;
+        printPattern(rows);
+    }
+
+    private static void printPattern(int rows) {
+        for (int i = 1; i <= rows; i++){
+            for (int j = i; j >= 1; j--){
+                System.err.print(j+" ");
+            }
+            System.err.println();
+        }
+    }
+}
