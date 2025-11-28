a22780a948ef993e42e03c8603d7e0dcc85ca2d9
AkshayM-Tech <akshay.k.mache@gmail.com>
2025-11-28 16:34:31 +0530

    add number pattern pattern22

---
+++ src/main/java/patterns/number/Pattern22.java
@@ -0,0 +1,29 @@
+package patterns.number;
+
+/* Write a java program to print below pattern
+1 2 3 4 5 6 7
+ 2 3 4 5 6 7
+  3 4 5 6 7
+   4 5 6 7
+    5 6 7
+     6 7
+      7
+*/
+public class Pattern22 {
+    public static void main(String[] args) {
+        int rows = 7;
+        printPattern(rows);
+    }
+
+    private static void printPattern(int rows) {
+        for (int i = 1; i <= rows; i++){
+            for (int j = i; j > 1; j--){
+                System.err.print(" ");
+            }
+            for (int k = i; k <= rows; k++){
+                System.err.print(k+" ");
+            }
+            System.err.println();
+        }
+    }
+}
