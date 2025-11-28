7b577f84903f64eec6244d4a875d08f6983708c7
AkshayM-Tech <akshay.k.mache@gmail.com>
2025-11-28 16:25:05 +0530

    add number pattern pattern19

---
+++ src/main/java/patterns/number/Pattern19.java
@@ -0,0 +1,29 @@
+package patterns.number;
+
+/* Write a java program to print below pattern
+      1
+     2 2
+    3 3 3
+   4 4 4 4
+  5 5 5 5 5
+ 6 6 6 6 6 6
+7 7 7 7 7 7 7
+*/
+public class Pattern19 {
+    public static void main(String[] args) {
+        int rows = 7;
+        printPattern(rows);
+    }
+
+    private static void printPattern(int rows) {
+        for (int i = 1; i <= rows; i++){
+            for (int j = i; j <= rows; j++){
+                System.err.print(" ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.err.print(i+" ");
+            }
+            System.err.println();
+        }
+    }
+}
