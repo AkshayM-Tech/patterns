6d39505ccc3560f3e0d4dd5cf915d7591fc1c17f
AkshayM-Tech <akshay.k.mache@gmail.com>
2025-11-28 15:38:55 +0530

    add number pattern pattern15

---
+++ src/main/java/patterns/number/Pattern15.java
@@ -0,0 +1,29 @@
+package patterns.number;
+
+/* Write a java program to print below pattern
+7 6 5 4 3 2 1
+  6 5 4 3 2 1
+    5 4 3 2 1
+      4 3 2 1
+        3 2 1
+          2 1
+            1
+*/
+public class Pattern15 {
+    public static void main(String[] args) {
+        int rows = 7;
+        printPattern(rows);
+    }
+
+    private static void printPattern(int rows) {
+        for (int i = rows; i >= 1; i--){
+            for (int j = i; j < rows; j++){
+                System.err.print("  ");
+            }
+            for (int k = i; k >= 1; k--){
+                System.err.print(k+" ");
+            }
+            System.err.println();
+        }
+    }
+}
